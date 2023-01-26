6d3c82bfcfbf706dd9556dfcc4541d7c2905454d
Rajan Maurya <therajanmaurya@users.noreply.github.com>
2023-01-25 21:36:30 -0500

    fix: Add Nasa response model

---
+++ app/src/main/java/com/nasa/obvious/models/Nasa.kt
@@ -0,0 +1,19 @@
+package com.nasa.obvious.models
+
+import com.google.gson.annotations.SerializedName
+
+data class Nasa(
+    val copyright: String,
+    val date: String,
+    val explanation: String,
+    val hdurl: String,
+
+    @SerializedName("media_type")
+    val mediaType: String,
+
+    @SerializedName("service_version")
+    val serviceVersion: String,
+
+    val title: String,
+    val url: String
+)
