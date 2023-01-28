53b30fd5e49ce60cf51fd31401d44991da168ba6
Rajan Maurya <therajanmaurya@users.noreply.github.com>
2023-01-28 14:37:22 -0500

    Fix: setup instrumentation test with hilt environment

--- app/src/main/java/com/nasa/obvious/ui/ImageViewModel.kt
+++ app/src/main/java/com/nasa/obvious/ui/ImageViewModel.kt
@@ -5,11 +5,13 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.nasa.obvious.data.Repository
 import com.nasa.obvious.models.Nasa
+import com.nasa.obvious.testing.OpenForTesting
 import com.nasa.obvious.utils.subscribeSingleOnMain
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
+@OpenForTesting
 class ImageViewModel @Inject constructor(
     private val repository: Repository,
     savedStateHandle: SavedStateHandle
